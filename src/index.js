aad04eaea701f7815f630cae88ea2fbdbe68a587
vshlg <varyasheleg14@gmail.com>
2019-03-19 12:28:15 +0300

    Add index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+
+module.exports = function getLoveTrianglesCount(arr) {
+    arr = arr.map((x) => x-1);
+
+    let loveTriangleCount=0
+
+    for(var i = 0; i < arr.length; i++) {
+        var next = arr[i];
+
+        if(next < 0 || next == i) {
+            continue;
+        }
+
+        var nextnext = arr[next];
+
+        if(nextnext < 0 || nextnext == next || nextnext == i) {
+            arr[i] = -1;
+            continue;
+        }
+
+        if(arr[nextnext] < 0) {
+            arr[next] = -1;
+            arr[i] = -1;
+            continue;
+        }
+
+        if(arr[nextnext] === i) {
+            loveTriangleCount++;
+        }
+
+        arr[i] = -1;
+    }
+    return loveTriangleCount;
+}
